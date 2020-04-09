e0316111bbe93bd9b7d02588d306e1b9eebc1f45
geodan89 <geodan89@gmail.com>
2020-04-09 19:56:25 +0300

    Initial project add.

---
+++ src/main/java/geo/springframework/geopetclinic2/GeoPetClinic2Application.java
@@ -0,0 +1,13 @@
+package geo.springframework.geopetclinic2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoPetClinic2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoPetClinic2Application.class, args);
+    }
+
+}
---
+++ src/test/java/geo/springframework/geopetclinic2/GeoPetClinic2ApplicationTests.java
@@ -0,0 +1,13 @@
+package geo.springframework.geopetclinic2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoPetClinic2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
