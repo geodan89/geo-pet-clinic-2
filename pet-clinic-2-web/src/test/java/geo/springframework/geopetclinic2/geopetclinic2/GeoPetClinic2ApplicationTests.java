4d0c31f241e4375dfdd15c984e148f77505ac225
geodan89 <geodan89@gmail.com>
2020-04-15 19:54:04 +0300

    Refactoring to multi-module maven. Closes #1

--- pet-clinic-2-web/src/test/java/geo/springframework/geopetclinic2/geopetclinic2/GeoPetClinic2ApplicationTests.java
+++ pet-clinic-2-web/src/test/java/geo/springframework/geopetclinic2/geopetclinic2/GeoPetClinic2ApplicationTests.java
@@ -1,8 +1,10 @@
-package geopetclinic2;
+package geo.springframework.geopetclinic2.geopetclinic2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+@ContextConfiguration
 @SpringBootTest
 class GeoPetClinic2ApplicationTests {
 
