9946e542bc14ea0a51d5df91f9eb66f179aee675
geodan89 <geodan89@gmail.com>
2020-04-18 21:18:22 +0300

    implement map service. closes #6

---
+++ pet-clinic-2-data/src/main/java/geo/springframework/geopetclinic2/services/map/OwnerServiceMap.java
@@ -0,0 +1,35 @@
+package geo.springframework.geopetclinic2.services.map;
+
+import geo.springframework.geopetclinic2.model.Owner;
+import geo.springframework.geopetclinic2.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll(){
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id){
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object){
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id){
+        super.deleteById(id);
+    }
+
+}
---
+++ pet-clinic-2-data/src/main/java/geo/springframework/geopetclinic2/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package geo.springframework.geopetclinic2.services.map;
+
+import geo.springframework.geopetclinic2.model.Pet;
+import geo.springframework.geopetclinic2.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
