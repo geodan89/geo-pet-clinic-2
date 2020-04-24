242f51e72000fd7e4d800629fc03bfe0f493f0e8
geodan89 <geodan89@gmail.com>
2020-04-24 19:08:01 +0300

    Created Pet Type, Pet and Visit Entities. Closes #26

--- pet-clinic-2-data/src/main/java/geo/springframework/geopetclinic2/model/PetType.java
+++ pet-clinic-2-data/src/main/java/geo/springframework/geopetclinic2/model/PetType.java
@@ -1,4 +1,14 @@
 package geo.springframework.geopetclinic2.model;
 
 public class PetType extends BaseEntity{
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
