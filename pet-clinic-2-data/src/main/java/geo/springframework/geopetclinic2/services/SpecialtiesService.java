347a58deb167a38423c4cf296ac72ade11d940f3
geodan89 <geodan89@gmail.com>
2020-04-25 15:53:10 +0300

    Created Specialty Map Impl Service. Closes #31

---
+++ pet-clinic-2-data/src/main/java/geo/springframework/geopetclinic2/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package geo.springframework.geopetclinic2.services;
+
+import geo.springframework.geopetclinic2.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
