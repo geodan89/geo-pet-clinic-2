6ad7cedc7737aac189faeda9c3f1e5d4124251b9
geodan89 <geodan89@gmail.com>
2020-04-25 15:14:19 +0300

    Created PetType Map Impl Service . Closes #29

---
+++ pet-clinic-2-data/src/main/java/geo/springframework/geopetclinic2/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package geo.springframework.geopetclinic2.services;
+
+import geo.springframework.geopetclinic2.model.PetType;
+
+public interface PetTypeService extends  CrudService<PetType, Long> {
+}
