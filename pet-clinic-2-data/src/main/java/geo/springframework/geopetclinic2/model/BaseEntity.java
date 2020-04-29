8969fbeb5e16564644c8e80a08016cec0f0d98ab
geodan89 <geodan89@gmail.com>
2020-04-29 20:53:12 +0300

    Implement Base Entity with JPA annotation. Closes #35

--- pet-clinic-2-data/src/main/java/geo/springframework/geopetclinic2/model/BaseEntity.java
+++ pet-clinic-2-data/src/main/java/geo/springframework/geopetclinic2/model/BaseEntity.java
@@ -1,16 +1,22 @@
 package geo.springframework.geopetclinic2.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+
+@Getter
+@Setter
+@MappedSuperclass //through this annotation we tell JPA this a super class from which other classes will inherit, thus we don't need this specific class mapped to the DB.
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
