8f9a52bbeb5ba21b3d4d55711f6dd758359e82c2
geodan89 <geodan89@gmail.com>
2020-04-29 21:10:06 +0300

    Convert Owners to JPA Entities. Closes #36

--- pet-clinic-2-data/src/main/java/geo/springframework/geopetclinic2/model/Person.java
+++ pet-clinic-2-data/src/main/java/geo/springframework/geopetclinic2/model/Person.java
@@ -1,8 +1,14 @@
 package geo.springframework.geopetclinic2.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity{
 
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
